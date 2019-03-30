c6df49590df3905cb6fdfe95204d87079eec113f
Matteoarnoux <48968472+Matteoarnoux@users.noreply.github.com>
2019-03-30 11:32:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+bot.on('ready',function() {
+    bot.user.setGame('AlbanyraRP, !help');
+    console.log('Le bot a bien été connecter');
+});
+
+bot.login(process.env.TOKEN);
